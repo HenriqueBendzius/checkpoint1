3d3ab0cd916b3aeb388569f0903bdece69d76914
HenriqueBendzius <116569544+HenriqueBendzius@users.noreply.github.com>
2024-04-04 21:40:24 -0300

    Adicionando exercicio 3
    Adicionando exercicio 3

---
+++ exercicio3/tipos.ts
@@ -0,0 +1,8 @@
+let numero:number = 0;
+let texto:string = "FUlano da Silva";
+let ativo:boolean = true;
+let arrayNumeros: number[] = [1,2,56];
+let pessoa: {nome: string, idade: number} ={
+    nome: "Beltrano",
+    idade: 87
+}

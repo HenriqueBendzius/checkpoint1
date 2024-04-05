0b5c051cb97cd5a1d0b6ffe0c4069e85781350de
alicecalisti <alicecalisti@yahoo.com.br>
2024-04-04 21:31:18 -0300

    Adicionando os componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
+import { MeuComponenteComponent } from './components/meu-componente/meu-componente.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MeuComponenteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'projeto-angular';
+  mensagem = 'Olá Mundo';
 }
